aff35e7b8a1c43c071fc47443d1784dcf1574506
Jarek <jaroslaw.trzybinski@gmail.com>
2020-09-16 16:58:54 +0100

    desktop design

--- src/App.js
+++ src/App.js
@@ -14,25 +14,30 @@ function App() {
       <main>
       {cards.map(card => (
         <div className="Card" key={card.id}>
-          <img
-            src={require(`${card.logo}`)}
-          />
-          <div className="CardTop">
-            <span className="CompanyName">{card.company} </span>
-            
-            {card.new && <span className="New">NEW!</span> }
-            {card.featured && <span className="Featured">FEATURES!</span> }
+          <div className="CardInfos">
+            <img
+              src={require(`${card.logo}`)}
+            />
+            <div className="CardWrapper">
+              <div className="CardTop">
+                <span className="CompanyName">{card.company} </span>
+                
+                {card.new && <span className="New">NEW!</span> }
+                {card.featured && <span className="Featured">FEATURES!</span> }
+              </div>
+              <div className="Position">
+                {card.position}
+              </div>
+              <div className="JobInfo">
+                <span>{card.postedAt}</span>
+                <span className="dot">&middot;</span>
+                <span>{card.contract}</span>
+                <span className="dot">&middot;</span>
+                <span>{card.location}</span>
+              </div>
+              </div>
           </div>
-          <div className="Position">
-            {card.position}
-          </div>
-          <div className="JobInfo">
-            <span>{card.postedAt}</span>
-            <span className="dot">&middot;</span>
-            <span>{card.contract}</span>
-            <span className="dot">&middot;</span>
-            <span>{card.location}</span>
-          </div>
+
           <div className="JobKnowledge">
             <span>{card.role}</span>
             <span>{card.level}</span>
