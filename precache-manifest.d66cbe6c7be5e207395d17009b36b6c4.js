2dbb45b78555d2a898f21a84afbb87d7807a7441
Jarek <jaroslaw.trzybinski@gmail.com>
2020-10-01 14:35:23 +0100

    Updates

---
+++ precache-manifest.d66cbe6c7be5e207395d17009b36b6c4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814dbe7e7695ee3a9ff2c92d9bed5a2d",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/index.html"
+  },
+  {
+    "revision": "3941148062289a0209ad",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/css/main.6be5a17f.chunk.css"
+  },
+  {
+    "revision": "f495ecfeb297885b29db",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/js/2.04abfe44.chunk.js"
+  },
+  {
+    "revision": "349a1a8e960b3e799f5df22fa2e8de3a",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/js/2.04abfe44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3941148062289a0209ad",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/js/main.44357e50.chunk.js"
+  },
+  {
+    "revision": "136c38c7bef08f743f08",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/js/runtime-main.abfc68b8.js"
+  },
+  {
+    "revision": "8fc9da643165a23264b3bdb08015f294",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/README.8fc9da64.md"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "e2e3cc6b844b754c3c5e36c413285f12",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/active-states.e2e3cc6b.jpg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "a49054ffcdf48235925ab11e47071124",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/desktop-design.a49054ff.jpg"
+  },
+  {
+    "revision": "1b35d4fb3bcd3cf2e1e4455f72dea0fa",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/desktop-preview.1b35d4fb.jpg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "ec46e5ea66f3f1a593f717702cdd741b",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/mobile-design.ec46e5ea.jpg"
+  },
+  {
+    "revision": "640ab81ebedb86deccd273eae964d800",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/mobile-with-filters.640ab81e.jpg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "04aebdb0dd7987f448e8d546e6ae0d4d",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/style-guide.04aebdb0.md"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/front-end-challenge-009-static-job-listings-master-reactjs/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
